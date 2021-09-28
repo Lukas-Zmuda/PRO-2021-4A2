07ed30f5cc52feae56a7d5d21b8cdc77f0bc6497
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-09-28 10:14:04 +0200

    datove typy

---
+++ Test/src/DatoveTypy.java
@@ -0,0 +1,55 @@
+
+public class DatoveTypy {
+
+	public static void main(String[] args) {
+
+		//celociselne
+		int a = 12;
+		
+		int bin = 0b10000000;
+		System.out.println("bin = " + bin);
+		int hex = 0xff;
+		System.out.println("hex = " + hex);
+		int oct = 0123;
+		System.out.println("oct = " + oct);
+		
+		byte b = 10;
+		int ii = b;
+		
+		
+		//pretypovanie
+		b = (byte)ii;
+		
+		//desatinne cisla
+		float f = 12.4f;
+		double dd = 456.4;
+		
+		
+		//znak
+		char acko = 'A';
+		
+		acko = 65;
+		
+		char uni = '\u0123';
+		System.out.println("acko = " + acko);
+		System.out.println(uni);
+		
+		//logicky
+		boolean prsi = false;//true
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
--- Test/src/MojaPrva.java
+++ Test/src/MojaPrva.java
@@ -2,6 +2,32 @@ public class MojaPrva {
 	
 	public static void main(String[] args) {
 		System.out.println("Ahoj 4A");
+		
+		System.out.println("lukas");
+		System.out.println("\"lukas\"");
+		System.out.println("\\lu\tkas\\");
+		
+		System.out.printf("Presny cas %d:%d:%d\n", 9, 34, 10);
+		System.out.println("tralala");
+		
+		System.out.println("10" + "5");
+		System.out.println(10 + 5);
+		
+		System.out.println("10" + 5);
+		
+		System.out.println("10 + 5 = " + (10 + 5));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 }
