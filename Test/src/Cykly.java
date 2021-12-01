9e2a95cc01f4ff9911708953807a83997de50fe2
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-12-01 08:29:37 +0100

    cyklus for

---
+++ Test/src/Cykly.java
@@ -0,0 +1,31 @@
+
+public class Cykly {
+
+	public static void main(String[] args) {
+		
+		//cyklus s pevnym poctom opakovani
+		for(int i = 0; i < 5; i++) {
+			System.out.println((i + 1) + ". Java");
+		}
+		
+		for(int i = 10; i > 0; i--) {
+			System.out.print(i + ",");
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < 11; i += 2) {
+			System.out.print(i + ",");
+		}
+		
+		System.out.println();
+		
+		for(int i = 1, j = 10; i < 11; i++, j--) {
+			System.out.println(i + ":" + j);
+		}
+		
+		
+		
+	}
+
+}
